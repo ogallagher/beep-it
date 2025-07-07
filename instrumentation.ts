d3c37348517c9c08a299736cf47feebaf7cf4502
Owen Gallagher <owengall@icloud.com>
2025-07-06 23:07:50 -0400

    expose readme in instrumentation #7
    instrumentation instead of npm run prestart with symlinks that are not compatible with compiled next site

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,13 +1,32 @@
-/**
- * If not running in forever, rename this method to register to launch the game server through instrumentation.
- */
-export async function register_skip() {
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
-        // pipe nextjs framework logger into pino
-        // await require('pino')
-        // await require('next-logger')
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    // pipe nextjs framework logger into pino
+    const pino = (await import('pino')).default
+    // await require('next-logger')
 
-        // launch game server
-        (await import('@api/server')).default()
-    }
+    const logger = pino({
+      name: 'instrumentation'
+    })
+
+    // launch game server (done in separate process)
+    // (await import('@api/server')).default()
+
+    // expose readme.md
+    const fs = (await import('fs/promises')).default
+    const readme = await fs.readFile('readme.md', { encoding: 'utf-8' })
+
+    logger.info('expose readme')
+    fs.writeFile(
+      'public/readme.md',
+      readme.replaceAll('public/', '/'),
+      { encoding: 'utf-8' }
+    )
+
+    logger.info('expose doc/ for readme')
+    fs.cp('doc', 'public/doc', {
+      errorOnExist: false,
+      force: true,
+      recursive: true
+    })
+  }
 }
