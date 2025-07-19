774a2e74675c2ac4581e672b051fec881078b519
Owen Gallagher <owengall@icloud.com>
2025-07-19 12:30:39 -0400

    define favicon #67

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { Suspense } from 'react';
+import { Suspense } from 'react'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
+/**
+ * Populates site metadata in `head.meta` tags.
+ */
 export const metadata: Metadata = {
-  title: "Beep It",
-  description: "Highly customizable virtual multiplayer party game inspired by Bop It.",
-};
+  title: 'Beep It',
+  description: 'Highly customizable virtual multiplayer party game inspired by Bop It.',
+  appleWebApp: {
+    title: 'BeepIt'
+  }
+}
 
 export default function RootLayout({
   children,
@@ -37,5 +43,5 @@ export default function RootLayout({
           creditsUrl='https://github.com/ogallagher'/> */}
       </body>
     </html>
-  );
+  )
 }
